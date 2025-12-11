be4b6f54195a243abe6cd5c008741f58fd15b978
hallvardbjordalsbakke <hallvard.bjordalsbakke@digdir.no>
2025-12-11 09:22:07 +0100

    initial commit

---
+++ src/main/java/no/minid/digdirleaderboard/DigdirLeaderboardApplication.java
@@ -0,0 +1,13 @@
+package no.minid.digdirleaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigdirLeaderboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigdirLeaderboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/minid/digdirleaderboard/DigdirLeaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package no.minid.digdirleaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigdirLeaderboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
